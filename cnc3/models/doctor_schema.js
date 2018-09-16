7a2edbe9a071452303e949ccd8d028076ce1a6bc
DeViL3998 <adarsh.brata@gmail.com>
2018-09-16 11:31:33 +0530

    final

--- cnc3/models/doctor_schema.js
+++ cnc3/models/doctor_schema.js
@@ -5,7 +5,64 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var doctor_schema = new Schema ({
-
+        dNumber:{
+            type: String
+        },
+        number: {
+            type: Number
+        },
+        email: {
+            type: String
+        },
+        address: {
+            type: String
+        },
+        pincode: {
+            type: Number
+        },
+        password: {
+            type: String
+        },
+        ENT: {
+            type: Number
+        },
+        Surgery: {
+            type: Number
+        },
+        Medicine: {
+            type: Number
+        },
+        SkinVD: {
+            type: Number
+        },
+        gynaecology: {
+            type: Number
+        },
+        Orthopedics: {
+            type: Number
+        },
+        Paediatric: {
+            type: Number
+        },
+        // normal ends here-------------------------------------
+        Gastroenterology: {
+            type: Number
+        },
+        Neurology: {
+            type: Number
+        },
+        Neurosurgery: {
+            type: Number
+        },
+        PlasticSurgery: {
+            type: Number
+        },
+        Nephrology: {
+            type: Number
+        },
+        Urology: {
+            type: Number
+        },
 });
 
 var doctor_schema = mongoose.model('doctor_schema', doctor_schema);
--- cnc3/routes/index.js
+++ cnc3/routes/index.js
@@ -6,37 +6,36 @@ var url = require('url');
 var formDataToObject = require("form-data-to-object");
 
 var users = require('../models/user_schema');
-var hospitals = require('../models/hospital_schema')
+var hospitals = require('../models/hospital_schema');
+var doctors = require('../models/doctor_schema');
 var mongoose = require('mongoose');
 
-
-router.get('/',function (req, res, next) {
+//===================================================================ok
+router.get('/api/',function (req, res, next) {
   res.sendFile(path.join(__dirname,"../public/index.html"));
 });
 
 
-//LOGIN
-router.get('/login', function(req, res, next) {
-  res.send('hello');
+//LOGIN===============================================================ok
+router.get('/api/login', function(req, res, next) {
+  res.sendFile(path.join(__dirname,'../public/login.html'));
 });
-/* GET home page. */
-router.post('/login', function(req, res, next) {
-  res.send(req.body.email);
-  if((req.body.email == "adarsh.brata@gmail.com" && req.body.password=="adarsh") || (req.body.email == "dikshant.brahma@gmail.com" && req.body.password=="dikshant")){
-    res.send("hello")
+//===================================================================
+router.post('/api/login', function(req, res, next) {
+  
+    res.redirect('/api/hospital/search');
     //res.render('profile', {password :req.password});
-  } else {
-    res.send('not welcome')
-  }
+  
 });
 
 
-//SIGNUP------------------------------------------------------------------------------------------------------------------
-router.get('/signup', function (req, res) {
-  res.sendFile(path.join(__dirname,'../public/signup.html'))
+//SIGNUP------------------------------------------------------------------ok
+router.get('/api/signup', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/signup.html'))
 });
-
-router.post('/signup', function (req, res) {
+//======================================================================ok
+router.post('/api/signup', function (req, res) {
+  
 
     console.log(req.body.email);
     var newUser = new users({
@@ -59,48 +58,56 @@ router.post('/signup', function (req, res) {
       });
 });
 
+
+
 //SEARCH HOSPITAL---------------------------------------------------------------------------------------------------------
-router.get('/hospital/search', function (req, res, next) {
-  
-  res.render('demosearch', {name : "Adarsh"})
+router.get('/api/hospital/search', function (req, res, next) {
 
-  
-  // hospitals.findBy({
-
-  // });
-
-
+ res.render('demosearch2',{});
 });
 
-router.post('/hospital/search', function (req, res, next) {
-  console.log(req.body);
+
+
+
+//============================================================================================
+router.post('/api/hospital/search', function (req, res, next) {
   const query = req.body.checkbox.reduce((a, x) => ({ ...a,
     [x]: {
       '$gte': 0
     }
   }), {});
+  let hosp ;
 
-  console.log(query);
 
   hospitals.find(query)
     .then((data) => {
-      console.log(data);
+      hosp = data;
+      console.log("hosp data"+hosp);
     })
     .catch(err => {
       res.send(err)
     })
-
-  res.render('layout', {});
-})
+  console.log(hosp);
+  for(i= 0; i<hosp.length; i++){
+    sum = 0
+    for(let key of req.body.checkbox){
+      sum += hosp[i][key];
+      console.log(sum);
+    }
+    hosp[i]["sum"] = sum;
+  }
+  console.log(hosp);
+  res.render('demosearch', {hosp} );
+}) 
 
 
 
 //Register Hospitals -----------------------------------------------------
-router.get('/hospital/register', function (req, res, next) {
+router.get('/api/hospital/register', function (req, res, next) {
   res.sendFile(path.join(__dirname,'../public/hosp_form.html'));
 });
 
-router.post('/hospital/register', function (req, res, next) {
+router.post('/api/hospital/register', function (req, res, next) {
   console.log(req.body)
   var newHosp = new hospitals({
     h_name: req.body.h_name,
@@ -146,16 +153,79 @@ router.post('/hospital/register', function (req, res, next) {
 });
 
 //SEARCH DOCTOR------------------------------------------------------------------------------------------------------------
-router.get('/doctor/search', function (req, res, next) {
-  var arr = {}
-  // doctors.findAll()
-  //   .then((data) => {
-  //     arr = data;
-  //   })
-  //   .catch(err=>{
-  //     res.send(err)
-  //   })
-  // res.render('docSearch',arr)
+router.get('/api/doctor/search', function (req, res, next) {
+  res.render('docSearch',{})
 })
+
+//post======================================================================
+router.post('/api/doctor/search', function (req, res, next) {
+  doc = {};
+  const query = req.body.checkbox.reduce((a, x) => ({ ...a,
+    [x]: {
+      '$gte': 0
+    }
+  }), {});
+
+  console.log(query);
+
+  doctors.find(query)
+    .then((data) => {
+      doc = data
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  sum = 0;
+  for(i= 0; i<doc.length; i++){
+    sum = 0
+    for(j=0; j<req.body.checkbox.length; j++){
+
+    }
+
+  }
+
+  res.render('demosearch', doc);
+})
+
+
+
+//Register doctor==================================================================
+router.get('/api/doctor/register', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/hosp_form.html'));
+});
+
+router.post('/api/hospital/register', function (req, res, next) {
+  console.log(req.body)
+  var newDoc = new doctors({
+    dname: req.body.dname,
+    number: req.body.number,
+    email: req.body.email,
+    password: req.body.password,
+    address: req.body.address,
+    pincode: req.body.pincode,
+    ENT: req.body.ENT,
+    Surgery: req.body.Surgery,
+    Medicine: req.body.Medicine,
+    SkinVD: req.body.SkinVD,
+    gynaecology: req.body.gynaecology,
+    Orthopedics: req.body.Orthopedics,
+    Paediatric: req.body.Paediatric,
+    Gastroenterology: req.body.Gastroenterology,
+    Neurology: req.body.Neurology,
+    Neurosurgery: req.body.Neurosurgery,
+    PlasticSurgery: req.body.PlasticSurgery,
+    Nephrology: req.body.Nephrology,
+    Urology: req.body.Urology,
+  });
+
+  newDoc.save()
+    .then((RES) => {
+      console.log(RES);
+      res.sendFile(path.join(__dirname, '../public/weWillVerify2.html'))
+    })
+    .catch(err => {
+      res.send(err)
+    })
+});
 
 module.exports = router;
